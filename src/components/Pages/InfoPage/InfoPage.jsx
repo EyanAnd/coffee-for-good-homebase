5c273f106bb70cd9dfc41f62fe36d2ce4514e263
Eyan Anderson <eyananderson@gmail.com>
2023-06-20 14:41:01 -0500

    added buttons and inputs for contact page

--- src/components/Pages/InfoPage/InfoPage.jsx
+++ src/components/Pages/InfoPage/InfoPage.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import { useState } from 'react';
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
 function InfoPage() {
+  // use state to make the submission form
+  const [submission, setSubmission] = useState({user_id: '', name: '', email: '', subject: '', description: ''});
+  // import dispatch to send it to a saga
+  // click handler to send dispatch to saga in order to send the submission to the backend
+  const addSubmission = () => {
+    console.log(submission)
+  }
   return (
     <div className="container">
-      <p>Info Page</p>
+      <p>Contact Us</p>
+      <label htmlFor="name">Name :<input type="text" value={submission.name} onChange={(e) => setSubmission({...submission, name: e.target.value})}/></label>
+      <label htmlFor="email">Email :<input type="text" value={submission.email} onChange={(e) => setSubmission({...submission, email: e.target.value})}/></label>
+      <label htmlFor="subject">Subject :<input type="text" value={submission.subject} onChange={(e) => setSubmission({...submission, subject: e.target.value})}/></label>
+      <label htmlFor="description">Description :<input type="text" value={submission.description} onChange={(e) => setSubmission({...submission, description: e.target.value})}/></label>
+      <button onClick={addSubmission}>Submit</button>
     </div>
   );
 }
--- src/components/Pages/UserPage/UserPage.jsx
+++ src/components/Pages/UserPage/UserPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import LogOutButton from '../../Shared/LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  console.log(user)
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <p>Application Status: STATUS HERE</p>
+      <button onClick={() => console.log('This will take you to the application route')} className='app-btn'>Start Application</button>
       <LogOutButton className="btn" />
     </div>
   );
