46f5ad90f1a99fd4cfb1c92581c5f10998d218d0
Eyan Anderson <eyananderson@gmail.com>
2023-06-28 22:12:00 -0500

    added simplistic styling to register page

--- src/components/Pages/RegisterPage/RegisterPage.jsx
+++ src/components/Pages/RegisterPage/RegisterPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import RegisterForm from './RegisterForm';
-import { Container, Button } from '@chakra-ui/react';
+import { Container, Button, Flex } from '@chakra-ui/react';
 
 function RegisterPage() {
-  const history = useHistory();
 
   return (
-    <Container>
-      <RegisterForm />
-    </Container>
+    <Flex paddingTop={'18rem'} height={'100vh'} gap={'4rem'} maxH={'100%'} justifyContent={'center'} >
+        <RegisterForm />
+    </Flex>
   );
 }
 
